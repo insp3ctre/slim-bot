95af2c333377761d2bd970d2be7aeae3a9cabf13
insp3ctre <67237263+insp3ctre@users.noreply.github.com>
2022-06-01 11:50:56 -0700

    virtual environment

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
-# test
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://college.lclark.edu/offices/health_promotion_and_wellness/covid-resources/list/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id = "lw_widget_b03fe531")
+
+elements = results.find_all("div", class_="table-cell")
+
+for element in elements:
+	current_case = element.find("span", class_ = "lw_profiles_667")
+	if current_case == None:
+		continue
+	print(current_case.text.strip())
+	break
